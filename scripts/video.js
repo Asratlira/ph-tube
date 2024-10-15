887e5008b769807d819789caca4c1aae10c73044
Asratlira <asratjananlira@gmail.com>
2024-10-15 11:42:14 +0600

    api section

---
+++ scripts/video.js
@@ -0,0 +1,16 @@
+const loadCatagories = () => {
+  fetch("https://openapi.programming-hero.com/api/phero-tube/categories")
+    .then((res) => res.json())
+    .then((data) => displayCategories(data.categories));
+};
+
+const displayCategories = (categories) => {
+  const categorieContainer = document.getElementById("categories");
+  categories.forEach((item) => {
+    const button = document.createElement("button");
+    button.classList = "btn";
+    button.innerText = item.category;
+    categorieContainer.append(button);
+  });
+};
+loadCatagories();
